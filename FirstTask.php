4cc218eb76545e9b30edd57e5f50acce195966a2
AsmaOdeh <asmaodeh73@gmail.com>
2017-04-30 17:35:55 +0300

    Create FirstTask.php

---
+++ FirstTask.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+
+<body>
+<?php
+   
+
+   $conn = mysqli_connect("localhost", "root", "","task1db");
+ 
+   
+ 
+  $result= $conn->query( "SELECT * FROM blog");
+  
+  if(!$result){
+  die (mysqli_error());
+  } else {
+  while($row = mysqli_fetch_array($result)){
+  echo $row['title']."____".$row['body']."____".$row['created_at']."<br>";
+  }
+  } 
+   
+?>
+</body>
+
+</html>
